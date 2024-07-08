6da05c24392a13e696ae39f5980bc9874044e4d3
imaniwilliam2 <154285022+imaniwilliam2@users.noreply.github.com>
2024-07-08 14:20:37 -0400

    all test passing

--- javascript/reverse_string.js
+++ javascript/reverse_string.js
@@ -1,9 +1,19 @@
 function reverseString(str) {
-  // type your code here
+  // Initialize an empty string to hold the reversed string
+  let reversedStr = '';
+
+  // Iterate through the input string from the end to the beginning
+  for (let i = str.length - 1; i >= 0; i--) {
+    // Append each character to the reversed string
+    reversedStr += str[i];
+  }
+
+  // Return the reversed string
+  return reversedStr;
 }
 
 if (require.main === module) {
-  // add your own tests in here
+  // Add your own tests in here
   console.log("Expecting: 'ih'");
   console.log("=>", reverseString("hi"));
 
@@ -11,9 +21,21 @@ if (require.main === module) {
 
   console.log("Expecting: 'ybabtac'");
   console.log("=>", reverseString("catbaby"));
+
+  console.log("");
+
+  console.log("Expecting: ''");
+  console.log("=>", reverseString(""));
+
+  console.log("");
+
+  console.log("Expecting: 'a'");
+  console.log("=>", reverseString("a"));
+
+  console.log("");
+
+  console.log("Expecting: 'dcba'");
+  console.log("=>", reverseString("abcd"));
 }
 
 module.exports = reverseString;
-
-// Please add your pseudocode to this file
-// And a written explanation of your solution
